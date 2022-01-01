ccbbfc9957950d1c479b913b0ae18ce5f8f9576d
Bach Nguyen <ntbdev1@gmail.com>
2022-01-01 18:14:11 +0100

    Update configs.
    Signed-off-by: Bach Nguyen <ntbdev1@gmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-mkdir -p ~/workspace
-git clone git@github.com:ntbdev/dotfiles.git ~/workspace
-ln -s ~/workspace/dotfiles/.config ~/testconfig/.config
+mkdir -p ~/utility
+[[ ! -f ~/utility/dotfiles ]] || git clone https://github.com/ntbdev/dotfiles.git ~/utility
+
+PROGRAMS=(
+	bspwm
+	sxhkd
+	alacritty
+	starship
+	nvim
+	tmux
+)
+
+for p in ${PROGRAMS[@]}; do
+	echo $p
+done
