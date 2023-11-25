c7c5e34a42fdb9e8e3517907e7f53b863124e949
moppediert <moppediert@gmail.com>
2023-11-25 20:13:10 +0100

    update nvim
    Signed-off-by: moppediert <moppediert@gmail.com>

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -61,20 +61,23 @@ cmp.setup({
     ['<C-e>'] = cmp.mapping.abort(),
     ['<Up>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
     ['<Down>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
     ['<C-k>'] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item({behavior = 'insert'})
-      else
-        cmp.complete()
       end
     end),
     ['<C-j>'] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_next_item({behavior = 'insert'})
-      else
-        cmp.complete()
       end
     end),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
   }),
   snippet = {
     expand = function(args)
