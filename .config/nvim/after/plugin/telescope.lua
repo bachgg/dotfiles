34b5d52b398cf032fd2b0f42c36bbfc03f5b9507
moppediert <moppediert@gmail.com>
2023-12-04 21:36:17 +0100

    update
    Signed-off-by: moppediert <moppediert@gmail.com>

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,30 +1,33 @@
 local builtin = require('telescope.builtin')
+
+-- vim.ui.select = 'telescope'
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>gr', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<leader><Space>', builtin.buffers, {})
 
 local actions = require('telescope.actions')
 
-require('telescope').setup{
-  pickers = {
-    buffers = {
-      initial_mode = "normal",
-      mappings = {
-        n = {
-            ["d"] = actions.delete_buffer
+require('telescope').setup {
+    pickers = {
+        buffers = {
+            initial_mode = "normal",
+            mappings = {
+                n = {
+                    ["d"] = actions.delete_buffer
+                }
+            }
         }
-      }
-    }
-  },
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous
-      }
-    }
-  },
+    },
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous
+            }
+        }
+    },
 }
