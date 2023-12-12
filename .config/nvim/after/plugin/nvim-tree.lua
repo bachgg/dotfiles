e310c3d4b3660216640c0538836ef415a85000d6
moppediert <moppediert@gmail.com>
2023-12-12 21:31:00 +0100

    nvim tree has built in function to focus file lol
    Signed-off-by: moppediert <moppediert@gmail.com>

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -8,7 +8,9 @@ require("nvim-tree").setup({
         sorter = "case_sensitive",
     },
     view = {
-        width = 30,
+        width = 45,
+        number = true,
+        relativenumber = true
     },
     renderer = {
         group_empty = true,
@@ -16,34 +18,15 @@ require("nvim-tree").setup({
     filters = {
         dotfiles = false,
     },
+    update_focused_file = {
+        enable = true,
+        update_root = false,
+        ignore_list = {},
+    },
 })
 
-local treeutils = require("treeutils")
+local api = require 'nvim-tree.api'
 
-
-vim.keymap.set('n', '<c-f>', treeutils.launch_find_files, {})
-vim.keymap.set('n', '<c-g>', treeutils.launch_live_grep, {})
-
-function find_directory_and_focus()
-    local actions = require("telescope.actions")
-    local action_state = require("telescope.actions.state")
-
-    local function open_nvim_tree(prompt_bufnr, _)
-        actions.select_default:replace(function()
-            local api = require("nvim-tree.api")
-
-            actions.close(prompt_bufnr)
-            local selection = action_state.get_selected_entry()
-            api.tree.open()
-            api.tree.find_file(selection.cwd .. "/" .. selection.value)
-        end)
-        return true
-    end
-
-    require("telescope.builtin").find_files({
-        find_command = { "fd", "--type", "directory", "--hidden", "--exclude", ".git/*" },
-        attach_mappings = open_nvim_tree,
-    })
-end
-
-vim.keymap.set("n", "<leader>fd", find_directory_and_focus)
+vim.keymap.set('n', '<leader>tr', function()
+    api.tree.toggle({ path = "<args>", update_root = false, find_file = true, focus = true, })
+end, {})
