1edf20534de2edd7b265d2f02d491b473f1cf1e2
moppediert <moppediert@gmail.com>
2023-12-16 19:07:31 +0100

    update

--- .config/nvim/after/plugin/conform.lua
+++ .config/nvim/after/plugin/conform.lua
@@ -1,18 +1,18 @@
 require("conform").setup({
-    formatters_by_ft = {
-        json = { { "prettierd", "prettier" } },
-        lua = { "stylua" },
-        python = { "black" },
-        javascript = { { "prettierd", "prettier" } },
-        javascriptreact = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
-        typescriptreact = { { "prettierd", "prettier" } },
-        terraform = { "terraform_fmt" },
+  formatters_by_ft = {
+    json = { { "prettierd", "prettier" } },
+    lua = { "stylua" },
+    python = { "black" },
+    javascript = { { "prettierd", "prettier" } },
+    javascriptreact = { { "prettierd", "prettier" } },
+    typescript = { { "prettierd", "prettier" } },
+    typescriptreact = { { "prettierd", "prettier" } },
+    terraform = { "terraform_fmt" },
 
-    },
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_fallback = true,
-    },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 })
--- .config/nvim/after/plugin/floaterm.lua
+++ .config/nvim/after/plugin/floaterm.lua
@@ -1,3 +1,7 @@
 vim.keymap.set('n', '<leader>ft', '<cmd>FloatermNew --cwd=<buffer-root><cr>', {})
 vim.keymap.set('n', '<leader>git', '<cmd>FloatermNew --cwd=<buffer-root> lazygit<cr>', {})
 
+vim.g.floaterm_width = 0.6
+vim.g.floaterm_height = 0.6
+vim.g.floaterm_title = ''
+vim.g.floaterm_opener = 'edit'
