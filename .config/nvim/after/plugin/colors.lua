3874993cf7fd1537f5cd55e0caaa892d87cf0856
moppediert <moppediert@gmail.com>
2023-12-17 08:30:27 +0100

    more

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,88 +1,88 @@
 -- Default options:
 require('kanagawa').setup({
-    compile = false,  -- enable compiling the colorscheme
-    undercurl = true, -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = {},
-    keywordStyle = { italic = true },
-    statementStyle = { bold = true },
-    typeStyle = {},
-    transparent = true,    -- do not set background color
-    dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-    terminalColors = true, -- define vim.g.terminal_color_{0,17}
-    colors = {             -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-    },
-    overrides = function(colors) -- add/modify highlights
-        return {}
-    end,
-    theme = "dragon",  -- Load "wave" theme when 'background' option is not set
-    background = {     -- map the value of 'background' option to a theme
-        dark = "wave", -- try "dragon" !
-        light = "lotus"
-    },
+  compile = false,  -- enable compiling the colorscheme
+  undercurl = true, -- enable undercurls
+  commentStyle = { italic = true },
+  functionStyle = {},
+  keywordStyle = { italic = true },
+  statementStyle = { bold = true },
+  typeStyle = {},
+  transparent = true,    -- do not set background color
+  dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+  terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  colors = {             -- add/modify theme and palette colors
+    palette = {},
+    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  },
+  overrides = function(colors) -- add/modify highlights
+    return {}
+  end,
+  theme = "dragon", -- Load "wave" theme when 'background' option is not set
+  background = {    -- map the value of 'background' option to a theme
+    dark = "wave",  -- try "dragon" !
+    light = "lotus"
+  },
 })
 
 require('ayu').setup({
-    overrides = {
-        Normal = { bg = "None" },
-        ColorColumn = { bg = "None" },
-        SignColumn = { bg = "None" },
-        Folded = { bg = "None" },
-        FoldColumn = { bg = "None" },
-        CursorLine = { bg = "None" },
-        CursorColumn = { bg = "None" },
-        WhichKeyFloat = { bg = "None" },
-        VertSplit = { bg = "None" },
-    },
-    mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
-    background = {
-        dark = 'ayu'
-    },
+  overrides = {
+    Normal = { bg = "None" },
+    ColorColumn = { bg = "None" },
+    SignColumn = { bg = "None" },
+    Folded = { bg = "None" },
+    FoldColumn = { bg = "None" },
+    CursorLine = { bg = "None" },
+    CursorColumn = { bg = "None" },
+    WhichKeyFloat = { bg = "None" },
+    VertSplit = { bg = "None" },
+  },
+  mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+  background = {
+    dark = 'ayu'
+  },
 })
 
 require("onedarkpro").setup({})
 
 require('lualine').setup {
-    options = {
-        icons_enabled = true,
-        theme = 'onedark',
-        component_separators = { left = '|', right = '|' },
-        section_separators = { left = '', right = '' },
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        }
+  options = {
+    icons_enabled = true,
+    theme = 'onedark',
+    component_separators = { left = '|', right = '|' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
     },
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { '' },
-        lualine_y = { 'filesize' },
-        lualine_z = { 'location' }
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { '' },
+    lualine_y = { 'filesize' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
 
 vim.cmd.colorscheme("kanagawa")
--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,140 +1,140 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    local opts = { buffer = bufnr }
-    lsp_zero.default_keymaps(opts)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  local opts = { buffer = bufnr }
+  lsp_zero.default_keymaps(opts)
 
-    vim.keymap.set('n', 'ga', function()
-        vim.lsp.buf.code_action()
-    end, opts)
+  vim.keymap.set('n', 'ga', function()
+    vim.lsp.buf.code_action()
+  end, opts)
 
-    vim.keymap.set('n', 'gn', function()
-        vim.diagnostic.goto_next()
-    end, opts)
+  vim.keymap.set('n', 'gn', function()
+    vim.diagnostic.goto_next()
+  end, opts)
 
-    vim.keymap.set('n', 'gp', function()
-        vim.diagnostic.goto_prev()
-    end, opts)
+  vim.keymap.set('n', 'gp', function()
+    vim.diagnostic.goto_prev()
+  end, opts)
 
-    vim.keymap.set('n', 'gr', function()
-        require('telescope.builtin').lsp_references({
-            preview = {
-                hide_on_startup = false
-            },
-            initial_mode = "normal",
-            sorting_strategy = "ascending"
-        })
-    end, opts)
+  vim.keymap.set('n', 'gr', function()
+    require('telescope.builtin').lsp_references({
+      preview = {
+        hide_on_startup = false
+      },
+      initial_mode = "normal",
+      sorting_strategy = "ascending"
+    })
+  end, opts)
 end)
 
 lsp_zero.set_sign_icons({
-    error = '✘',
-    warn = '▲',
-    hint = '⚑',
-    info = '»'
+  error = '✘',
+  warn = '▲',
+  hint = '⚑',
+  info = '»'
 })
 
 require('lspconfig').lua_ls.setup {
-    settings = {
-        Lua = {
-            runtime = {
-                version = 'LuaJIT'
-            },
-            diagnostics = {
-                globals = { 'vim' }
-            },
-            workspace = {
-                library = {
-                    vim.env.VIMRUNTIME,
-                }
-            },
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT'
+      },
+      diagnostics = {
+        globals = { 'vim' }
+      },
+      workspace = {
+        library = {
+          vim.env.VIMRUNTIME,
         }
+      },
     }
+  }
 }
 
 require('lspconfig').eslint.setup({
-    on_attach = function(client, bufnr)
-        vim.keymap.set('n', '<leader>es', function()
-            vim.api.nvim_command('EslintFixAll')
-        end)
-    end,
+  on_attach = function(client, bufnr)
+    vim.keymap.set('n', '<leader>es', function()
+      vim.api.nvim_command('EslintFixAll')
+    end)
+  end,
 })
 
 require('rust-tools').setup({
-    tools = {
-        parameter_hints_prefix = "◀︎ ",
-        inlay_hints = {
-            other_hints_prefix = "▶︎ "
-        }
+  tools = {
+    parameter_hints_prefix = "◀︎ ",
+    inlay_hints = {
+      other_hints_prefix = "▶︎ "
     }
+  }
 })
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = {
-        'tsserver',
-        'rust_analyzer',
-        'lua_ls',
-        'bashls',
-        'cssls',
-        'jsonls',
-        'marksman',
-        'sqlls',
-        'tflint',
-        'yamlls',
-        'tailwindcss',
-        'vimls',
-        'pyright',
-        'volar',
-    },
-    handlers = {
-        lsp_zero.default_setup,
-        tsserver = function()
-            require('lspconfig').tsserver.setup({
-                settings = {
-                    completions = {
-                        completeFunctionCalls = true
-                    }
-                }
-            })
-        end,
-    },
+  ensure_installed = {
+    'tsserver',
+    'rust_analyzer',
+    'lua_ls',
+    'bashls',
+    'cssls',
+    'jsonls',
+    'marksman',
+    'sqlls',
+    'tflint',
+    'yamlls',
+    'tailwindcss',
+    'vimls',
+    'pyright',
+    'volar',
+  },
+  handlers = {
+    lsp_zero.default_setup,
+    tsserver = function()
+      require('lspconfig').tsserver.setup({
+        settings = {
+          completions = {
+            completeFunctionCalls = true
+          }
+        }
+      })
+    end,
+  },
 })
 
 local cmp = require('cmp')
 
 cmp.setup({
-    sources = {
-        { name = 'nvim_lsp' },
-    },
-    mapping = cmp.mapping.preset.insert({
-        -- ['<Esc>'] = cmp.mapping.abort(),
-        ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
-        ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ['<C-k>'] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.select_prev_item({ behavior = 'select' })
-            end
-        end),
+  sources = {
+    { name = 'nvim_lsp' },
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- ['<Esc>'] = cmp.mapping.abort(),
+    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+    ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    ['<C-k>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item({ behavior = 'select' })
+      end
+    end),
 
-        ['<C-j>'] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.select_next_item({ behavior = 'select' })
-            end
-        end),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = false,
-        }),
+    ['<C-j>'] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item({ behavior = 'select' })
+      end
+    end),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = false,
     }),
-    snippet = {
-        expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
 })
