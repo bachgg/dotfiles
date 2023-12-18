b957161d9f89867148b758c899478a9df0cad845
moppediert <moppediert@gmail.com>
2023-12-18 08:49:38 +0100

    add search in file

--- .config/nvim/lua/default/remap.lua
+++ .config/nvim/lua/default/remap.lua
@@ -33,3 +33,6 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
+
+-- search
+vim.keymap.set("v", "<C-f>", "y/\\v<C-r>\"<CR>")
