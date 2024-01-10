d910375e906e75238b57367cca62912725f78ab0
moppediert <moppediert@gmail.com>
2024-01-10 14:42:39 +0100

    update

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,9 +1,5 @@
 return {
   "rebelot/kanagawa.nvim",
-  "Shatur/neovim-ayu",
-  "olimorris/onedarkpro.nvim",
   "nvim-tree/nvim-web-devicons",
   { "nvim-lualine/lualine.nvim", requires = { 'nvim-tree/nvim-web-devicons', opt = true } },
-  { "olimorris/onedarkpro.nvim", priority = 100 },
-  { "catppuccin/nvim",           name = "catppuccin",                                     priority = 1000 }
 }
