a2ef65f8c55ebc332373776eb5422419c5590b9c
moppediert <moppediert@gmail.com>
2024-04-10 18:03:26 +0200

    update

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -3,6 +3,6 @@ return {
   "nvim-tree/nvim-web-devicons",
   { 'rose-pine/neovim',           name = 'rose-pine' },
   { "nvim-lualine/lualine.nvim",  requires = { 'nvim-tree/nvim-web-devicons', opt = true } },
-  { 'projekt0n/github-nvim-theme' }
-
+  { 'projekt0n/github-nvim-theme' },
+  { "cpea2506/one_monokai.nvim", }
 }
