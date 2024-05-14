ccdf01c6dcc9e9b1e83c270e1aa608e2d5ca7825
moppediert <moppediert@gmail.com>
2024-05-14 14:00:51 +0200

    update

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,8 +4,24 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+
+function vim.getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg('v')
+  vim.fn.setreg('v', {})
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ''
+  end
+end
+
 vim.keymap.set('v', '<leader>fj', function()
+  local text = vim.getVisualSelection()
   builtin.grep_string({
+    default_text = text,
     additional_args = {
       '--hidden'
     }
