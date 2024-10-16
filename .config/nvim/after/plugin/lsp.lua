a7411941e4d911f49a6531ce7db7a27e088d00c3
moppediert <moppediert@gmail.com>
2024-10-16 20:13:26 +0200

    border

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -170,6 +170,10 @@ cmp.setup({
       require('luasnip').lsp_expand(args.body)
     end,
   },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
