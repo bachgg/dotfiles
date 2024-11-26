3a2aab77e477c1a0aa65582c6dd7301ad72e6f82
moppediert <moppediert@gmail.com>
2024-11-26 21:09:25 +0100

    remove an if

--- .config/scripts/uai_repo.sh
+++ .config/scripts/uai_repo.sh
@@ -2,14 +2,13 @@
 
 PATH=$PATH:/opt/homebrew/bin
 DIR_PATH=$(ls -d $HOME/uai/workspace/*/ $HOME/workspace/*/ $HOME/dotfiles | fzf)
-if [ ! -z $DIR_PATH ]; then 
-  DIR_PATH=$(realpath $DIR_PATH)
-  DIR_NAME=$(basename $DIR_PATH)
-  if [ "${DIR_PATH#*uai}" != "$DIR_PATH" ]; then
-    SESSION="uai/$DIR_NAME"
-  else
-    SESSION="bach/$DIR_NAME"
-  fi
-  (tmux has-session -t "$SESSION" &> /dev/null || tmux new-session -c $DIR_PATH -s "$SESSION" -d) && tmux switch-client -t "$SESSION"
+[ -z $DIR_PATH ] && exit
+DIR_PATH=$(realpath $DIR_PATH)
+DIR_NAME=$(basename $DIR_PATH)
+if [ "${DIR_PATH#*uai}" != "$DIR_PATH" ]; then
+  SESSION="uai/$DIR_NAME"
+else
+  SESSION="bach/$DIR_NAME"
 fi
+(tmux has-session -t "$SESSION" &> /dev/null || tmux new-session -c $DIR_PATH -s "$SESSION" -d) && tmux switch-client -t "$SESSION"
 
