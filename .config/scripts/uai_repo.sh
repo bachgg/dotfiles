812cb45d2797dd5318686248562e00d993c3112d
moppediert <moppediert@gmail.com>
2024-11-26 21:02:04 +0100

    more dir

--- .config/scripts/uai_repo.sh
+++ .config/scripts/uai_repo.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 PATH=$PATH:/opt/homebrew/bin
-DIR_PATH=$(ls -d $HOME/uai/workspace/*/ | fzf)
+DIR_PATH=$(ls -d $HOME/uai/workspace/*/ $HOME/workspace/*/ | fzf)
 if [ ! -z $DIR_PATH ]; then 
   DIR_PATH=$(realpath $DIR_PATH)
   DIR_NAME=$(basename $DIR_PATH)
-  (tmux has-session -t $DIR_NAME &> /dev/null || tmux new-session -c $DIR_PATH -s $DIR_NAME -d) && tmux switch-client -t $DIR_NAME
+  if [ "${DIR_PATH#*uai}" != "$DIR_PATH" ]; then
+    SESSION="uai/$DIR_NAME"
+  else
+    SESSION="bach/$DIR_NAME"
+  fi
+  (tmux has-session -t "$SESSION" &> /dev/null || tmux new-session -c $DIR_PATH -s "$SESSION" -d) && tmux switch-client -t "$SESSION"
 fi
 
