b09fcc4c0e436092c39bf834779da827e16ba9ec
moppediert <moppediert@gmail.com>
2025-02-03 21:11:57 +0100

    now actually works

--- .config/scripts/mr.sh
+++ .config/scripts/mr.sh
@@ -7,30 +7,20 @@ BLUE="\\033[34m"
 CYAN="\\033[36m"
 RESET="\\033[0m"
 
-# get 10 latest MRs
-MR=$(glab mr list --per-page 10 --not-label renovate -F json -R $(git remote get-url origin))
+mr=$(glab mr list --per-page 10 --not-label renovate -F json -R $(git remote get-url origin)\
+  | jq -c 'map({repo: .references.full, iid, title,web_url, description, author: .author.username, target_branch, source_branch})')
 
-# magic parsing of the returned json
-PARSED=$(echo "$MR" | jq -R '.' | jq -s '.' | jq -r 'join("")')
+repo=$(git remote get-url origin | sed 's_.*/__; s/\.git$//')
 
-# repo slug
-REPO=$(echo $PARSED | jq -r '.[0] | "\(.references.full)"' | sed -E 's#.*/([^/]+)![0-9]+#\1#')
+messages=$(jq -r -n --argjson data "$mr" '$data | .[] | "!\(.iid) [\(.title)](\(.web_url)) @sre-team \(.description)"')
 
-# get `iid`s of MRs
-TITLES=$(echo $PARSED | jq -r '.[] | "!\(.iid) [\(.title)](\(.web_url)) @sre-team \(.description)"' | sed "s/^/\`$REPO\`/")
-
-# fzf entries
-FZF_ENTRIES=$(
-  echo $PARSED |
-  jq -r '.[] | "!\(.iid),\(.title),\(.author.username),(\(.target_branch)) ← (\(.source_branch))"' |
+fzf_entries=$(jq -r -n --argjson data "$mr" '$data | .[] | "!\(.iid),\(.title),\(.author),(\(.target_branch)) ← (\(.source_branch))"' |
   awk \
     -v red=$RED -v green=$GREEN -v blue=$BLUE -v cyan=$CYAN -v reset=$RESET \
     -F ',' '{
     printf "%-20s " green "%-70s" reset "\n" red "%-20s " blue "%-70s" reset "\\0", $1, $2, $3, $4;
-  }'
-)
+  }')
 
-SELECTED=$(echo "$FZF_ENTRIES"  | fzf --ansi --read0 | sed -n '1p')
-IID=$(echo "$SELECTED" | sed -E 's/^(![0-9]+).*$/\1/')
-echo $TITLES | grep $IID | sed -E 's/^(.+)(![0-9]+)(.*)(@sre-team)(.*)$/\1\3\4\n\5/' | pbcopy
+selected_iid=$(echo $fzf_entries  | fzf --ansi --read0 | sed -n '1p' | sed -E 's/^!([0-9]+).*$/\1/')
 
+jq -r -n --arg repo "$repo" --argjson iid "$selected_iid" --argjson data "$mr" '$data | .[] | select(.iid == $iid) | "`\($repo)` [\(.title)](\(.web_url)) @sre-team\n\(.description)"' | pbcopy
