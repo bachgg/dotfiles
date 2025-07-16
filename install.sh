afb4d5833ca0032522cf5e96d738460bf9d8d588
moppediert <moppediert@gmail.com>
2025-07-16 19:06:20 +0200

    t

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@ set -e
 dotfiles_dir="${HOME}/dotfiles"
 
 if [ ! -d "${dotfiles_dir}" ]; then
-  echo "Cloning script repository..."
-  pacman -Sy --noconfirm git
-  git clone https://github.com/moppediert/dotfiles.git ${dotfiles_dir}
+  pacman -Sy --noconfirm git gum
+  gum spin --spinner dot --show-output --title "Updating databases..." -- pacman -Syyu
+  gum spin --spinner dot --show-output --title "Cloning repo..." -- git clone https://github.com/moppediert/dotfiles.git ${dotfiles_dir}
   sh ${dotfiles_dir}/install.sh
 else
   echo "Running installation script..."
