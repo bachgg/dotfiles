dbf934bc00c3aa19ba614942f7c828c51af25a1c
moppediert <moppediert@gmail.com>
2025-07-17 21:04:39 +0200

    install from file instead

--- archinstall.sh
+++ archinstall.sh
@@ -1,13 +1,9 @@
-# curl -fsSL moppediert.github.io/dotfiles/archinstall.sh | sh
+# iwctl station wlan0 connect wifi_name
 
-# read -p "Wifi network: " wifi_network
-# iwctl station wlan0 connect "${wifi_network}"
-if [ ! -e archinstall.sh ]; then
-  curl -fsSLO moppediert.github.io/dotfiles/archinstall.sh
-  curl -fsSLO moppediert.github.io/dotfiles/user_configuration.json
-  curl -fsSLO moppediert.github.io/dotfiles/user_credentials.json
-else
-  echo "Archinstall password: "
-  read -s archinstall_password <&1 # https://unix.stackexchange.com/a/510807
-  archinstall --config user_configuration.json --creds user_credentials.json --creds-decryption-key "${archinstall_password}"
-fi
+# curl -fsSL0 moppediert.github.io/dotfiles/archinstall.sh && sh -e archinstall.sh
+
+curl -fsSLO moppediert.github.io/dotfiles/user_configuration.json
+curl -fsSLO moppediert.github.io/dotfiles/user_credentials.json
+echo "Archinstall password: "
+read -s archinstall_password
+archinstall --config user_configuration.json --creds user_credentials.json --creds-decryption-key "${archinstall_password}"
