dd94875785b06371b1be3865c4f106b63fa13a96
moppediert <moppediert@gmail.com>
2025-07-17 20:25:27 +0200

    stdin doesn't work over internet script

--- archinstall.sh
+++ archinstall.sh
@@ -2,9 +2,12 @@
 
 # read -p "Wifi network: " wifi_network
 # iwctl station wlan0 connect "${wifi_network}"
-curl -fsSLO moppediert.github.io/dotfiles/user_configuration.json
-curl -fsSLO moppediert.github.io/dotfiles/user_credentials.json
-
-echo "Archinstall password: "
-read -s archinstall_password
-archinstall --config user_configuration.json --creds user_credentials.json --creds-decryption-key "${archinstall_password}"
+if [ ! -e archinstall.sh ]; then
+  curl -fsSLO moppediert.github.io/dotfiles/archinstall.sh
+  curl -fsSLO moppediert.github.io/dotfiles/user_configuration.json
+  curl -fsSLO moppediert.github.io/dotfiles/user_credentials.json
+else
+  echo "Archinstall password: "
+  read -s archinstall_password
+  archinstall --config user_configuration.json --creds user_credentials.json --creds-decryption-key "${archinstall_password}"
+fi
