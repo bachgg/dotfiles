9961e3355daff7f3ff592151cab71440b2e443db
moppediert <moppediert@gmail.com>
2025-07-17 19:56:53 +0200

    proactive password

--- archinstall.sh
+++ archinstall.sh
@@ -4,4 +4,6 @@
 # iwctl station wlan0 connect "${wifi_network}"
 curl -fsSLO moppediert.github.io/dotfiles/user_configuration.json
 curl -fsSLO moppediert.github.io/dotfiles/user_credentials.json
-archinstall --config user_configuration.json --creds user_credentials.json
+
+read -s -p "Archinstall password: " archinstall_password
+archinstall --config user_configuration.json --creds user_credentials.json --creds-decryption-key "${archinstall_password}"
