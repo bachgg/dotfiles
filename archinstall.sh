d4eeeff45e60460b21ba4c03fb92f1b0db11642c
moppediert <moppediert@gmail.com>
2025-07-18 22:07:21 +0200

    add reboot

--- archinstall.sh
+++
@@ -1,9 +0,0 @@
-# iwctl station wlan0 connect wifi_name
-
-# curl -fsSLO moppediert.github.io/dotfiles/archinstall.sh && sh -e archinstall.sh
-
-curl -fsSLO moppediert.github.io/dotfiles/user_configuration.json
-curl -fsSLO moppediert.github.io/dotfiles/user_credentials.json
-echo "Archinstall password: "
-read -s archinstall_password
-archinstall --config user_configuration.json --creds user_credentials.json --creds-decryption-key "${archinstall_password}"
