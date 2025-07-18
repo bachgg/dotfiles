271aea3b834be59a74443b0daecbe699ff69f63c
moppediert <moppediert@gmail.com>
2025-07-18 20:38:36 +0200

    fix: dir browsing works with dirs starting with a dot

--- .config/scripts/dirs.sh
+++ .config/scripts/dirs.sh
@@ -3,6 +3,6 @@
 if [ $(uname) = "Darwin" ]; then PATH=${PATH}:/opt/homebrew/bin:~/.fzf/bin; fi
 DIR_PATH=$(find ~/ ~/workspace ~/uai/workspace -maxdepth 1 -type d | fzf)
 [ -z ${DIR_PATH} ] && exit
-SESSION=$(echo ${DIR_PATH} | sed -e "s_${HOME}/__g")
+SESSION=$(echo ${DIR_PATH} | sed "s_${HOME}/__g" | sed "s/\./_/g")
 (tmux has-session -t "${SESSION}" &> /dev/null || tmux new-session -d -c ${DIR_PATH} -s "${SESSION}" "nvim ${DIR_PATH}" \; new-window -t ${SESSION}: -c ${DIR_PATH}) && tmux switch-client -t "${SESSION}"
 
