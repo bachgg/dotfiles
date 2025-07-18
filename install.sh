cdb9b4f1f3114905aedf080247440e75272d57bf
moppediert <moppediert@gmail.com>
2025-07-18 20:53:51 +0200

    combine install scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
-# curl -fsSL moppediert.github.io/dotfiles/install.sh | sh
+# sh <(curl -fsSL moppediert.github.io/dotfiles/install.sh)
+
+if [ -d /run/archiso ]; then
+  curl -fsSLO moppediert.github.io/dotfiles/user_configuration.json
+  curl -fsSLO moppediert.github.io/dotfiles/user_credentials.json
+  echo "Archinstall password: "
+  read -s archinstall_password
+  archinstall --config user_configuration.json --creds user_credentials.json --creds-decryption-key "${archinstall_password}"
+  exit 0
+fi
 
 dotfiles_dir="${HOME}/dotfiles"
 
@@ -6,9 +15,10 @@ if [ ! -d "${dotfiles_dir}" ]; then
   sudo pacman -Sy --noconfirm git
   git clone https://github.com/moppediert/dotfiles.git ${dotfiles_dir}
   sh -e ${dotfiles_dir}/install.sh
-else
-  echo "Running installation script..."
-  for script in $(ls ${dotfiles_dir}/installation/*); do
-    sh -e ${script}
-  done
+  exit 0
 fi
+
+echo "Running installation script..."
+for script in $(ls ${dotfiles_dir}/installation/*); do
+  sh -e ${script}
+done
