9964581160109aa6fb00fce57e9289058d49bc00
moppediert <moppediert@gmail.com>
2025-07-18 20:31:55 +0200

    remove more

--- .config/scripts/dirs.sh
+++ .config/scripts/dirs.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 if [ $(uname) = "Darwin" ]; then PATH=${PATH}:/opt/homebrew/bin:~/.fzf/bin; fi
-[ -d ~/workspace ] || mkdir -p ~/workspace/z
-[ -d ~/uai/workspace ] || mkdir -p ~/uai/workspace/z
 DIR_PATH=$(find ~/ ~/workspace ~/uai/workspace -maxdepth 1 -type d | fzf)
 [ -z ${DIR_PATH} ] && exit
 SESSION=$(echo ${DIR_PATH} | sed -e "s_${HOME}/__g")
