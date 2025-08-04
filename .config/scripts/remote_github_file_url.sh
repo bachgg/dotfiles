abf45232d040754b5fa75a0705fb74457475917b
bachgg <online@bach.gg>
2025-08-04 21:46:56 +0200

    copy github remote file url of current nvim node for ease of sharing links

---
+++ .config/scripts/remote_github_file_url.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+current_commit=$(git rev-parse HEAD)
+repo=$(git remote get-url origin | sed -E 's_git@github.com:(.+).git_\1_g')
+repo_root=$(git rev-parse --show-toplevel)
+file_path=${1}
+relative_file_path=$(echo ${file_path} | sed "s_${repo_root}/__g")
+if which wl-copy; then
+  copy_command=$(which wl-copy)
+else
+  copy_command=$(which pbcopy)
+fi
+
+printf "https://github.com/%s/blob/%s/%s" ${repo} ${current_commit} ${relative_file_path} | ${copy_command}
