494dacbfed2951b407563c2a03b92423654bd172
bachgg <online@bach.gg>
2025-08-18 17:49:11 +0200

    update some quality of life changes

--- .config/scripts/dirs.sh
+++ .config/scripts/dirs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $(uname) = "Darwin" ]; then PATH=${PATH}:/opt/homebrew/bin:~/.fzf/bin; fi
-DIR_PATH=$(find ~/ ~/workspace -maxdepth 1 -type d | fzf)
+DIR_PATH=$(find ~/ ~/workspace ~/workspace/codesphere-monorepo ~/workspace/codesphere-monorepo/helm -maxdepth 1 -type d | fzf)
 [ -z ${DIR_PATH} ] && exit
 SESSION=$(echo ${DIR_PATH} | sed "s_${HOME}/__g" | sed "s/\./_/g")
 (tmux has-session -t "${SESSION}" &> /dev/null || tmux new-session -d -c ${DIR_PATH} -s "${SESSION}" "nvim ${DIR_PATH}" \; new-window -t ${SESSION}: -c ${DIR_PATH}) && tmux switch-client -t "${SESSION}"
