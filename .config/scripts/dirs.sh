a08456b4a9451baa6d8618e5edaac44906ca6057
bachgg <online@bach.gg>
2025-08-21 10:29:15 +0200

    tmux has weird target-session matching mechanism

--- .config/scripts/dirs.sh
+++ .config/scripts/dirs.sh
@@ -4,5 +4,7 @@ if [ $(uname) = "Darwin" ]; then PATH=${PATH}:/opt/homebrew/bin:~/.fzf/bin; fi
 DIR_PATH=$(find ~/ ~/workspace ~/workspace/codesphere-monorepo ~/workspace/codesphere-monorepo/helm -maxdepth 1 -type d | fzf)
 [ -z ${DIR_PATH} ] && exit
 SESSION=$(echo ${DIR_PATH} | sed "s_${HOME}/__g" | sed "s/\./_/g")
-(tmux has-session -t "${SESSION}" &> /dev/null || tmux new-session -d -c ${DIR_PATH} -s "${SESSION}" "nvim ${DIR_PATH}" \; new-window -t ${SESSION}: -c ${DIR_PATH}) && tmux switch-client -t "${SESSION}"
+
+# target-session needs to be prefixed with an equal sign to get matched exactly https://man7.org/linux/man-pages/man1/tmux.1.html#COMMANDS
+(tmux has-session -t "=${SESSION}" &> /dev/null || tmux new-session -d -c ${DIR_PATH} -s "${SESSION}" "nvim ${DIR_PATH}" \; new-window -t ${SESSION}: -c ${DIR_PATH}) && tmux switch-client -t "${SESSION}"
 
