e232500be2bb2a486f3651395f15e050f4c6544c
bachgg <online@bach.gg>
2025-09-15 16:46:10 +0200

    export n prefix

--- .config/shell/export.sh
+++ .config/shell/export.sh
@@ -11,15 +11,16 @@ export XDG_CONFIG_HOME="$HOME/.config"
 export MANPAGER="sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
 
 if which op 2>&1 1>&/dev/null && [ -e ~/.config/op/plugins.sh ]; then
-   source ~/.config/op/plugins.sh
+  source ~/.config/op/plugins.sh
 fi
 
 # https://github.com/jeffreytse/zsh-vi-mode#execute-extra-commands
 zvm_after_init_commands+=('source <(fzf --zsh)')
-eval "$(op completion zsh)"; compdef _op op
+eval "$(op completion zsh)"
+compdef _op op
 
 # https://github.com/ohmyzsh/ohmyzsh/discussions/9849
-git_prompt_info(){}
+git_prompt_info() {}
 PROMPT="%y %{${fg_bold[red]}%}:: %{${fg[green]}%}%3~%(0?. . %{${fg[red]}%}%? )%{${fg[blue]}%}»%{${reset_color}%} "
 
 # gardenctl session
@@ -28,3 +29,5 @@ PROMPT="%y %{${fg_bold[red]}%}:: %{${fg[green]}%}%3~%(0?. . %{${fg[red]}%}%? )%{
 # [ -e ~/.kube ] || mkdir ~/.kube
 # [ -e ~/.garden/gardenctl-v2.yaml ] || ([ -e ~/.kube/kubeconfig-garden-codesphere.yaml ] && gardenctl config set-garden codesphere --kubeconfig ~/.kube/kubeconfig-garden-codesphere.yaml)
 # KUBECONFIG=$(ls -1 ~/.kube/kubeconfig-garden* | tr '\n' ':') kubectl config view --merge --flatten > ~/.kube/config
+
+export N_PREFIX=~/.local/
