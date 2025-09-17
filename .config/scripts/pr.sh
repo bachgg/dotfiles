7159a6833a75f876db862dbfdc3831f14e28a6f9
bachgg <online@bach.gg>
2025-09-17 18:52:44 +0200

    pr script

---
+++ .config/scripts/pr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if op --version 1>/dev/null 2>&1; then
+  clickup_token="$(op read --account Codesphere "op://Employee/Clickup/token")"
+  clickup_api_url="$(op read --account Codesphere "op://Employee/Clickup/sre-tasks")"
+fi
+
+if [ -z "$clickup_token" ]; then
+  echo Missing ClickUp token && exit 1
+fi
+
+clickup_tasks=$(curl -fsSL \
+  --request "GET" \
+  --url "${clickup_api_url}?assignees[]=93666470&statuses[]=in%20progress" \
+  --header "Authorization: $clickup_token" \
+  --header "accept: application/json")
+
+my_tasks="$(jq -r -n --argjson tasks "$clickup_tasks" \
+  '$tasks.tasks | .[] | [.id,.name] | @tsv')"
+
+task_id="$(echo "$my_tasks" | fzf --ansi | awk '{print $1}')"
+
+gh pr create -b CU-"$task_id" -e
