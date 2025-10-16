378ad0efb1728518fb69113fba63a53e67c51369
bachgg <online@bach.gg>
2025-10-16 20:21:44 +0200

    some git convenient

--- .config/scripts/repo.sh
+++ .config/scripts/repo.sh
@@ -3,4 +3,7 @@ set -e
 
 name="$1"
 if [ -z "$name" ]; then echo "Repo name missing" && exit 1; fi
-cd "$HOME/workspace" && gh repo create "$name" --private --clone
+cd "$HOME/workspace"
+gh repo create "$name" --private --clone
+cd "$HOME/workspace/$name"
+git remote set-url origin "$(git remote get-url origin | sed 's_https://github.com/_git@github.com:_g')"
